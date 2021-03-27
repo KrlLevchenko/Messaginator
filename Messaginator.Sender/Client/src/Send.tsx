6b548dd3dc6f09a5e52291a46c6cf731b3fe48f7
Kirill Levchenko <k.levchenko@dodopizza.com>
2021-03-27 18:01:26 +0300

    fix build

--- Messaginator.Sender/Client/src/Send.tsx
+++ Messaginator.Sender/Client/src/Send.tsx
@@ -1,4 +1,4 @@
-﻿import React, { ChangeEvent, FormEventHandler, useState } from 'react'
+﻿import React, { ChangeEvent, useState } from 'react'
 import { Box, Button, CircularProgress, createStyles, TextField, Theme } from '@material-ui/core'
 import sendClient, { SendMessageRequest } from './ApiClient/sendClient'
 import { makeStyles } from '@material-ui/core/styles'
